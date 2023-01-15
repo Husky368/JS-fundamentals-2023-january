11e371fda656c3a549b95cdddd48c6140e9b6731
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2023-01-15 10:17:53 +0200

    Update 08-arrayManipulation.js

--- 05-arraysAdvanced/lab/08-arrayManipulation.js
+++ 05-arraysAdvanced/lab/08-arrayManipulation.js
@@ -1,4 +1,28 @@
-function arrayManipulation(declarationg){
-    let array = declarationg[0]
-    console.log(array);
+function arrayManipulation(declaration) {
+    let arrayNumbers = declaration.shift().split(" ").map(Number)
+    for (i = 0; i < declaration.length; i++) {
+        let currentLine = declaration[i].split(' ')
+        let operation = currentLine[0]
+        let num = Number(currentLine[1])
+        switch (operation) {
+            case 'Add':
+                arrayNumbers.push(num)
+                break;
+            case 'Remove':
+                for (j = 0; j < arrayNumbers.length; j++) {
+                    if(arrayNumbers[j]==num){
+                        arrayNumbers.slice(arrayNumbers[j-1],arrayNumbers[j+1])
+                    }
+                }
+                break;
+            case 'RemoveAt':
+                break;
+            case 'Inser':
+                break;
+            default:
+                break;
+        }
+    }
+    // console.log(declarationg);
+
 }
