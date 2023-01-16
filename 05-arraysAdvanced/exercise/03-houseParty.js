7c08700f7793fd37a8f24d8d030f494b699e3b9e
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2023-01-16 16:53:46 +0200

    Update 03-houseParty.js

--- 05-arraysAdvanced/exercise/03-houseParty.js
+++ 05-arraysAdvanced/exercise/03-houseParty.js
@@ -1,4 +1,24 @@
 function houseParty(declaration){
-    
-
+    let guestList = []
+    for (let el of declaration) {
+        let command = el.split(' ')
+        if(command.length==3){
+            if(guestList.includes(command[0])){
+                console.log(`${command[0]} is already in the list!`);
+            }
+            else{
+                guestList.push(command[0])
+            }
+        }
+        else{
+            if(guestList.includes(command[0])){
+                let index = guestList.indexOf(command[0])
+                guestList.splice(index,1)
+            }
+            else{
+                console.log(`${command[0]} is not in the list!`)
+            }
+        }
+    }
+    console.log(guestList.join("\n"))
 }
