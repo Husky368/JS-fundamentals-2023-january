715e9cec76b533e7ba1327599ec9a6e20f077bfe
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2023-01-17 18:36:16 +0200

    Update 08-arrayManipulator.js

--- 05-arraysAdvanced/exercise/08-arrayManipulator.js
+++ 05-arraysAdvanced/exercise/08-arrayManipulator.js
@@ -4,19 +4,78 @@ function arrayManipulator(arrayNumbers, commands) {
         let operation = currentCommand[0]
         switch (operation) {
             case 'add':
+                let index = Number(currentCommand[1])
+                let num = Number(currentCommand[2])
+                for (i = arrayNumbers.length - 1; i >= index; i--) {
+                    arrayNumbers[i + 1] = arrayNumbers[i]
+                }
+                arrayNumbers.splice(index, 1, num)
                 break;
             case 'addMany':
+                let arr = currentCommand.map(Number)
+                let indexForMany = arr[1]
+                arr.splice(0, 2)
+                for (iTwo = arrayNumbers.length - 1; iTwo >= indexForMany; iTwo--) {
+                    arrayNumbers[iTwo + arr.length] = arrayNumbers[iTwo]
+                }
+                let doneN = 0
+                for (const el of arrayNumbers) {
+                    let indexEl = arrayNumbers.indexOf(el) + 1
+                    if (indexEl >= indexForMany) {
+                        arrayNumbers.splice(indexEl, 1, arr[doneN])
+                        doneN++
+                    }
+                    if (doneN >= arr.length) {
+                        break;
+                    }
+                }
                 break;
             case 'contains':
+                let lookedFor = Number(currentCommand[1])
+                let done = false
+                for (const el of arrayNumbers) {
+                    if (el == lookedFor) {
+                        let containsIndex = arrayNumbers.indexOf(el)
+                        console.log(containsIndex)
+                        done = true
+                        break;
+                    }
+                }
+                if(!done){
+                    console.log('-1')
+                }
                 break;
             case 'remove':
+                let removeIndex = Number(currentCommand[1])
+                arrayNumbers.splice(removeIndex, 1)
                 break;
             case 'shift':
+                let shiftIndex = Number(currentCommand[1])
+                for (j = 1; j <= shiftIndex; j++) {
+                    let shiftedNum = arrayNumbers.shift()
+                    arrayNumbers.push(shiftedNum)
+                }
                 break;
             case 'sumPairs':
+                let sumPairsArr = []
+                let sumPairsIndex = 0
+                let started = arrayNumbers.length
+                while(arrayNumbers.length>0){
+                    let firstN = arrayNumbers.shift()
+                    let secondN = 0
+                    if(arrayNumbers.length>=1){
+                        secondN = arrayNumbers.shift()
+                        sumPairsIndex++
+                    }
+                    let sum = firstN+ secondN
+                    sumPairsArr.push(sum)
+                    sumPairsIndex++
+                }
+                arrayNumbers = sumPairsArr
                 break;
-                case 'print':
-                    break;
+            case 'print':
+                console.log(`[ `+ arrayNumbers.join(", ")+` ]`)
+                break;
         }
     }
     // 2 - length= 1
@@ -24,6 +83,7 @@ function arrayManipulator(arrayNumbers, commands) {
     // 1  length = 3
     // 1 bezkrayno
 } arrayManipulator(
-    [1, 2, 4, 5, 6, 7],
-    ['add 1 8', 'contains 1', 'contains 3', 'print']
+    [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2],
+    ["sumPairs", "sumPairs", "addMany 0 -1 -2 -3", "print"]
 )
+//3 3 3 3 3 3 
