e7025f5d7172d4ea109a81ffa31f4ddb4b744bf4
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2023-01-18 21:11:47 +0200

    Create 04-biggest.js

---
+++ basixSyntax,ConditionalStatementsAndLoops/moreExercises/04-biggest.js
@@ -0,0 +1,10 @@
+function biggest(num,numTwo,nim){
+    let biggest = num
+    if(biggest<numTwo){
+        biggest = numTwo
+    }
+    if(biggest<nim){
+        biggest = nim
+    }
+    console.log(biggest);
+}
