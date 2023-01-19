dd10f44985fb9dda2cc455e541075ef08b1764cc
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2023-01-19 20:59:07 +0200

    Update 08-arrayManipulation.js

--- 05-arraysAdvanced/lab/08-arrayManipulation.js
+++ 05-arraysAdvanced/lab/08-arrayManipulation.js
@@ -1,28 +1,93 @@
-function arrayManipulation(declaration) {
-    let arrayNumbers = declaration.shift().split(" ").map(Number)
-    for (i = 0; i < declaration.length; i++) {
-        let currentLine = declaration[i].split(' ')
-        let operation = currentLine[0]
-        let num = Number(currentLine[1])
+function arrayManipulator(arrayNumbers, commands) {
+    for (i = 0; i < commands.length; i++) {
+        let currentCommand = commands[i].split(' ')
+        let operation = currentCommand[0]
         switch (operation) {
-            case 'Add':
-                arrayNumbers.push(num)
+            case 'add':
+                let index = Number(currentCommand[1])
+                let num = Number(currentCommand[2])
+                for (i = arrayNumbers.length - 1; i >= index; i--) {
+                    arrayNumbers[i + 1] = arrayNumbers[i]
+                }
+                arrayNumbers.splice(index, 1, num)
                 break;
-            case 'Remove':
-                for (j = 0; j < arrayNumbers.length; j++) {
-                    if(arrayNumbers[j]==num){
-                        arrayNumbers.slice(arrayNumbers[j-1],arrayNumbers[j+1])
+            case 'addMany':
+                let arr = currentCommand.map(Number)
+                let indexForMany = arr[1]
+                arr.splice(0, 2)
+                for (iTwo = arrayNumbers.length - 1; iTwo >= indexForMany; iTwo--) {
+                    arrayNumbers[iTwo + arr.length] = arrayNumbers[iTwo]
+                }
+                let doneN = 0
+                let startedZero = false
+                for (const el of arrayNumbers) {
+                    let indexEl = arrayNumbers.indexOf(el) + 1
+                    if(arrayNumbers.indexOf(el)==0){
+                        startedZero = true
+                    }
+                    if(startedZero){
+                        indexEl--
+                    }
+                    if (indexEl >= indexForMany) {
+                        arrayNumbers.splice(indexEl, 1, arr[doneN])
+                        doneN++
+                    }
+                    if (doneN >= arr.length) {
+                        break;
                     }
                 }
                 break;
-            case 'RemoveAt':
+            case 'contains':
+                let lookedFor = Number(currentCommand[1])
+                let done = false
+                for (const el of arrayNumbers) {
+                    if (el == lookedFor) {
+                        let containsIndex = arrayNumbers.indexOf(el)
+                        console.log(containsIndex)
+                        done = true
+                        break;
+                    }
+                }
+                if(!done){
+                    console.log('-1')
+                }
                 break;
-            case 'Inser':
+            case 'remove':
+                let removeIndex = Number(currentCommand[1])
+                arrayNumbers.splice(removeIndex, 1)
                 break;
-            default:
+            case 'shift':
+                let shiftIndex = Number(currentCommand[1])
+                for (j = 1; j <= shiftIndex; j++) {
+                    let shiftedNum = arrayNumbers.shift()
+                    arrayNumbers.push(shiftedNum)
+                }
+                break;
+            case 'sumPairs':
+                let sumPairsArr = []
+                let sumPairsIndex = 0
+                let started = arrayNumbers.length
+                while(arrayNumbers.length>0){
+                    let firstN = arrayNumbers.shift()
+                    let secondN = 0
+                    if(arrayNumbers.length>=1){
+                        secondN = arrayNumbers.shift()
+                        sumPairsIndex++
+                    }
+                    let sum = firstN+ secondN
+                    sumPairsArr.push(sum)
+                    sumPairsIndex++
+                }
+                arrayNumbers = sumPairsArr
+                break;
+            case 'print':
+                console.log(`[ `+ arrayNumbers.join(", ")+` ]`)
                 break;
         }
     }
-    // console.log(declarationg);
-
+    // 2 - length= 1
+    // 3 - length = 2
+    // 1  length = 3
+    // 1 bezkrayno
 }
+//3 3 3 3 3 3 
