be7d6b4b849983fb726392e1f783806a995a3fde
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2023-01-29 21:30:58 +0200

    Create 04-nonDecreasingNumbers.js

---
+++ arrays/moreExercises/04-nonDecreasingNumbers.js
@@ -0,0 +1,10 @@
+function nonDecreasingNumbers(arr) {
+    let max = arr[0];
+    let result = arr.filter(el => {
+        if (el >= max) {
+            max = el;
+        }
+        return el >= max;
+    });
+    console.log(result.join(' '));
+}
