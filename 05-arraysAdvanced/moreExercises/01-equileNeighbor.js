a1363a4d7cea57a9b7e6d61cf88f4b1994f0c473
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2023-02-09 23:09:02 +0200

    Update 01-equileNeighbor.js

--- 05-arraysAdvanced/moreExercises/01-equileNeighbor.js
+++ 05-arraysAdvanced/moreExercises/01-equileNeighbor.js
@@ -15,9 +15,4 @@ function equileNeighbor(declaration) {
         }
     }
     console.log(resultNum)
-} equileNeighbor(
-    [['2', '3', '4', '7', '0'],
-    ['4', '0', '5', '3', '4'],
-    ['2', '3', '5', '4', '2'],
-    ['9', '8', '7', '5', '4']]
-)
+}
