db2ac21b69ea7cc96af43411f92a649b47bbc78e
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2023-03-10 22:43:53 +0200

    Update 08-travelTime.js

--- 09-associativeArrays/exercises/08-travelTime.js
+++ 09-associativeArrays/exercises/08-travelTime.js
@@ -1,21 +1,33 @@
-function travelTime(declaration) {
+function travelTime(input) {
     let destinations = new Map();
-    for (let destination of declaration) {
-        let [country, city, cost] = destination.split(' > ')
+    for (let line of input) {
+        let [country, city, cost] = line.split(' > ');
         if (!destinations.has(country)) {
-            if (destinations[city].has(country)) {
-                destinations[country].add(cost)
-            }else{
-                destinations.set(city, new Set())
-                destination.set(city,country)
-            }
-        } else {
-
+            destinations.set(country, new Map());
         }
+        destinations.get(country).set(city, Number(cost));
+    }
+    let arrDestinations = Array.from(destinations);
+    arrDestinations = arrDestinations.sort((a, b) => a[0].localeCompare(b[0]));
+    for (let [countryName, value] of arrDestinations) {
+        let arrValue = Array.from(value)
+        if (arrValue.length > 1) {
+            let resLine = countryName + ' ->';
+            arrValue = arrValue.sort((a, b) => a[1] - b[1]);
+            for (const cityName of arrValue) {
+                resLine += ' ' + cityName[0] + ' -> ' + cityName[1]
+            }
+            console.log(resLine);
+            continue;
+        }
+        console.log(`${countryName} -> ${arrValue[0][0]} -> ${arrValue[0][1]}`)
     }
 } travelTime([
-    "Bulgaria > Sofia > 500",
-    "Bulgaria > Sopot > 800",
-    "France > Paris > 2000",
-    "Albania > Tirana > 1000",
-    "Bulgaria > Sofia > 200"])
+    'Bulgaria > Sofia > 25000',
+    'Bulgaria > Sofia > 25000',
+    'Kalimdor > Orgrimar > 25000',
+    'Albania > Tirana > 25000',
+    'Bulgaria > Varna > 25010',
+    'Bulgaria > Lukovit > 10'
+    ]
+    )
