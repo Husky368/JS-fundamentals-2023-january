e5fa3baa64d4e7f6db32d3823201fc3430473eef
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2023-03-25 06:16:38 +0200

    Update 09-needForSpeed.js

--- 12-finalExamPreparation/09-needForSpeed.js
+++ 12-finalExamPreparation/09-needForSpeed.js
@@ -1,24 +1,73 @@
 function needForSpeed(input) {
     let n = Number(input.shift());
-    let carsArr = [];
+    let carsMap = new Map();
+    //model, [miliage[0],fuel[1]]
     while (n > 0) {
         let line = input.shift();
         let [model, miliage, fuel] = line.split('|');
-        carsArr.push(model, [miliage, fuel]);
+        carsMap.set(model, [Number(miliage), Number(fuel)]);
         n--;
     }
     for (let line of input) {
-        let [command,model]
+        let info = line.split(' : ');
+        let command = info.shift();
+        switch (command) {
+            case 'Drive':
+                //"Not enough fuel to make that ride"
+                //"{car} driven for {distance} kilometers. {fuel} liters of fuel consumed."
+                let searchingModelDrive = info.shift();
+                let searchingDistanceDrive = Number(info.shift());
+                let searchingFuelDrive = Number(info.shift());
+                let currentDrive = carsMap.get(searchingModelDrive);
+                if (currentDrive[1] >= searchingFuelDrive) {
+                    console.log(`${searchingModelDrive} driven for ${searchingDistanceDrive} kilometers. ${searchingFuelDrive} liters of fuel consumed.`);
+                    currentDrive[0] += searchingDistanceDrive;
+                    currentDrive[1] -= searchingFuelDrive;
+                    carsMap.set(searchingModelDrive, currentDrive)
+                    if (currentDrive[0] >= 100000) {
+                        console.log(`Time to sell the ${searchingModelDrive}!`);
+                        carsMap.delete(searchingModelDrive);
+                        continue;
+                    }
+
+                } else {
+                    console.log(`Not enough fuel to make that ride`)
+                }
+                break;
+            case 'Refuel':
+                let refuelCarModel = info.shift();
+                let refuelLitters = Number(info.shift());
+                let currentRefuel = carsMap.get(refuelCarModel);
+                let beginingLitters = currentRefuel[1];
+                let littersFilled = refuelLitters + currentRefuel[1];
+                if (littersFilled > 75) {
+                    littersFilled = 75;
+                }
+                currentRefuel[1] = littersFilled;
+                littersFilled -= beginingLitters;
+                carsMap.set(refuelCarModel, currentRefuel)
+                console.log(`${refuelCarModel} refueled with ${littersFilled} liters`);
+                break;
+            case 'Revert':
+                //"{car} mileage decreased by {amount reverted} kilometers"
+                let revertCarModel = info.shift();
+                let revertKm = Number(info.shift());
+                let currentRevert = carsMap.get(revertCarModel);
+                currentRevert[0] -= revertKm;
+                if (currentRevert[0] < 10000) {
+                    currentRevert[0] = 10000;
+                    carsMap.set(revertCarModel, currentRevert);
+                    continue;
+                }
+                carsMap.set(revertCarModel, currentRevert);
+                console.log(`${revertCarModel} mileage decreased by ${revertKm} kilometers`)
+                break;
+            case 'Stop':
+                let carsArr = Array.from(carsMap);
+                for (const line of carsArr) {
+                    console.log(`${line[0]} -> Mileage: ${line[1][0]} kms, Fuel in the tank: ${line[1][1]} lt.`)
+                }
+                break;
+        }
     }
-} needForSpeed([
-    '3',
-    'Audi A6|38000|62',
-    'Mercedes CLS|11000|35',
-    'Volkswagen Passat CC|45678|5',
-    'Drive : Audi A6 : 543 : 47',
-    'Drive : Mercedes CLS : 94 : 11',
-    'Drive : Volkswagen Passat CC : 69 : 8',
-    'Refuel : Audi A6 : 50',
-    'Revert : Mercedes CLS : 500',
-    'Revert : Audi A6 : 30000',
-    'Stop'])
+}
