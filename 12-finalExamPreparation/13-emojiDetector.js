27f55778ec3d682d8645b52a72825b5e7f6e18b5
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2023-03-31 07:01:55 +0300

    Update 13-emojiDetector.js

--- 12-finalExamPreparation/13-emojiDetector.js
+++ 12-finalExamPreparation/13-emojiDetector.js
@@ -1,34 +1,34 @@
 function emojiDetector(input) {
     let regex = /([:*]{2})[A-Z][a-z]{2,}\1/g;
-    let regexD = /\d/g
+    let regexD = /\d/g;
     let matches = input[0].match(regex);
     let digits = input[0].match(regexD);
-    let coolThrescode = '';
+    let coolThreshold = 0;
     let resArr = [];
     for (let index = 0; index < digits.length; index++) {
         if (index == 0) {
-            coolThrescode = Number(digits[index]);
+            coolThreshold = Number(digits[index])
             continue;
         }
-        coolThrescode *= Number(digits[index])
+        coolThreshold *= Number(digits[index])
     }
+
     for (let ell of matches) {
         let firstW = ell;
         ell = ell.slice(2, ell.length - 2)
-        ell = ell.split('');
         let curr = 0;
-        for (let i = 0; i < ell.length; i++) {
-            let asciiNum = ell[i].charCodeAt()
+        for (const letter of ell) {
+            let asciiNum = letter.charCodeAt()
             curr += asciiNum
         }
-        if (curr >= coolThrescode) {
-            ell = ell.join('');
+        if (coolThreshold < curr) {
             resArr.push(firstW)
         }
     }
-    console.log(`Cool threshold: ${coolThrescode}`)
+    console.log(`Cool threshold: ${coolThreshold}`)
     console.log(`${matches.length} emojis found in the text. The cool ones are:`)
     for (const iterator of resArr) {
         console.log(iterator)
     }
-} emojiDetector(["5, 4, 3, 2, 1, go! The 1-th consecutive banana-eating contest has begun! ::Joy:: **Banana** ::Wink:: **Vali** ::valid_emoji::"])
+
+}emojiDetector()
