e253934c94101834a01e465c972b77046f0f1c9d
Krasimir Yuliyanov <73038532+Husky368@users.noreply.github.com>
2023-04-01 22:44:48 +0300

    Update 04-worldTour.js

--- 12-finalExamPreparation/04-worldTour.js
+++ 12-finalExamPreparation/04-worldTour.js
@@ -3,63 +3,50 @@ function worldTour(input) {
     let tripPlan = input.shift();
     for (let ellement of input) {
         ellement = ellement.split(':');
-        if (ellement[0] == 'Add Stop') {
+        let command = ellement.shift();
+        if (command == 'Add Stop') {
             // •	"Add Stop:{index}:{string}":
             // o	Insert the given string at that index only if the index is valid
-            if (Number(ellement[1]) >= 0 && Number(ellement[1]) < tripPlan.length) {
-                let indexAdd = Number(ellement[1]);
-                let newWordAdd = '';
-                let count = 0;
-                tripPlan.split('').forEach((letter) => {
-                    if (count == indexAdd) {
-                        newWordAdd += ellement[2] + letter;
-                    } else {
-                        newWordAdd += letter
-                    }
-                    count++;
-                });
-                tripPlan = newWordAdd;
-                console.log(tripPlan)
+            let indexAddStop = Number(ellement.shift());
+            let stringAddStop = ellement.shift();
+            tripPlan = tripPlan.split('');
+            if (indexAddStop >= 0 && indexAddStop < tripPlan.length) {
+                tripPlan.splice(indexAddStop, 0, stringAddStop)
             }
+            tripPlan = tripPlan.join('');
+            console.log(tripPlan)
         }
-        else if (ellement[0] == 'Remove Stop') {
+        else if (command == 'Remove Stop') {
             // •	"Remove Stop:{start_index}:{end_index}":
             // o	Remove the elements of the string from the starting index to the end index (inclusive) if both indices are valid
-            if (Number(ellement[1]) >= 0 && Number(ellement[2]) < tripPlan.length) {
-                let startIndex = Number(ellement[1]);
-                let endIndex = Number(ellement[2]);
-                let countLetters = Number(ellement[2]) - Number(ellement[1]);
+            let startIndex = Number(ellement.shift());
+            let endIndex = Number(ellement.shift());
+            if (startIndex >= 0 && endIndex < tripPlan.length) {
+                let cutCount = endIndex - startIndex + 1;
                 tripPlan = tripPlan.split('');
-                tripPlan.splice(startIndex, countLetters+1);
+                tripPlan.splice(startIndex, cutCount);
                 tripPlan = tripPlan.join('');
             }
             console.log(tripPlan);
         }
-        else if (ellement[0] == 'Switch') {
+        else if (command == 'Switch') {
             // •	"Switch:{old_string}:{new_string}":
             // o	If the old string is in the initial string, replace it with the new one (all occurrences)
-            let oldWordSwitch = ellement[1];
-            let newWordSwitch = ellement[2];
-            let regex = new RegExp(oldWordSwitch)
-            while (regex.test(tripPlan)) {
-                let info = regex.exec(tripPlan);
-                // tripPlan.slice(info.index,info.index+oldWordSwitch.length)
-                tripPlan = tripPlan.split('');
-                tripPlan.splice(info.index, info.index + oldWordSwitch.length,newWordSwitch);
-                tripPlan = tripPlan.join('');
+            let oldString = ellement.shift();
+            let newString = ellement.shift();
+            if (tripPlan.includes(oldString)) {
+                tripPlan = tripPlan.replace(oldString, newString);
             }
-            console.log(tripPlan)
+            console.log(tripPlan);
         }
-        else{
-            console.log(`Ready for world tour! Planned stops: ${tripPlan}`)
+        else {
             break;
         }
     }
-
+    console.log(`Ready for world tour! Planned stops: ${tripPlan}`)
 } worldTour
-    ((["Albania:Bulgaria:Cyprus:Deuchland",
-    "Add Stop:3:Nigeria",
-    "Remove Stop:4:8",
-    "Switch:Albania: Azərbaycan",
-    "Travel"])
-    )
+    (["Hawai::Cyprys-Greece",
+        "Add Stop:7:Rome",
+        "Remove Stop:11:16",
+        "Switch:Hawai:Bulgaria",
+        "Travel"])
